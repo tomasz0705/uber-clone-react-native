bf9934244bcb70c5fe19397a1cb782369a851e4c
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-12-28 21:49:52 +0100

    Added reducers to navSlice.js

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 // 1) Set up Redux
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets build Uber!</Text>
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text>Lets build Uber</Text>
+      </View>
+    </Provider>
+
   );
 }
 
