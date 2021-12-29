7b57d7bdbe2d440d64fb4ac4877f163c6988e21c
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-12-29 13:02:30 +0100

    Finished setting up Redux, added selectors to navSlice.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { store } from './store';
 
 // 1) Set up Redux
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Lets build Uber</Text>
       </View>
