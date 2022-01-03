550edd5df21c7bbd409d85ceffcabeb2119bb18f
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-01-03 20:08:48 +0100

    Added styles to TouchableOpacity element and style to Text Element

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import tw from 'twrnc';
 
 const data = [
   {
@@ -21,14 +22,15 @@ const NavOptions = () => {
       <FlatList
         data={data}
         keyExtractor={(item) => item.id}
-        // horizontal
+        horizontal
         renderItem={({ item }) => (
-            <TouchableOpacity>
+            <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
                 <View>
                     <Image
                         style={{ width: 120, height: 120, resizeMode: "contain" }}
                         source={{ uri: item.image }}
                     />
+                    <Text style={tw`mt-2 text-lg font-bold`}>{item.title}</Text>
                 </View>
             </TouchableOpacity>
         )}
