e6345611c924381d86ae4c16704be0804d5c1e0b
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-01-05 17:44:41 +0100

    Added native stac to App.js & react-navigation to NavOptions.js

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,7 +1,8 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { Icon } from "react-native-elements";
-import tw from 'twrnc';
+import tw from "twrnc";
 
 const data = [
   {
@@ -19,27 +20,34 @@ const data = [
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
+
   return (
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        horizontal
-        renderItem={({ item }) => (
-            <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-                <View>
-                    <Image
-                        style={{ width: 120, height: 120, resizeMode: "contain" }}
-                        source={{ uri: item.image }}
-                    />
-                    <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-                    <Icon
-                        style={tw`p-2 bg-black rounded-full w-10 mt-4`}
-                        name="arrowright" color="white" type="antdesign"
-                    />
-                </View>
-            </TouchableOpacity>
-        )}
-      />
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity
+            onPress={() => navigation.navigate(item.screen)}
+            style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              color="white"
+              type="antdesign"
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
   );
 };
 
