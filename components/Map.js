c9ce1f4325dcc6bd8667f5387fbf9f3df568de47
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-01-14 14:03:39 +0100

    Added MapView & Marker as child with attributes to Map.js

--- components/Map.js
+++ components/Map.js
@@ -1,19 +1,36 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+import { useSelector } from "react-redux";
 import tw from "twrnc";
+import { selectOrigin } from "../slices/navSlice";
 
 const Map = () => {
+  const origin = useSelector(selectOrigin);
+
   return (
     <MapView
       style={tw`flex-1`}
+      mapType="mutedStandard" //more subtle, makes markers/lines pop more (iOS 11.0+ only)
       initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
       }}
-    />
+    >
+      {origin?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+          title="Origin"
+          description={origin.description}
+          identifier="origin"
+        />
+      )}
+    </MapView>
   );
 };
 
