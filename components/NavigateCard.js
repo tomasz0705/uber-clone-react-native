d8c67ca5c404ddf9e0abbe12b966e43210af0f3a
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-01-17 16:13:55 +0100

    Added GooglePlacesAutocomplete to NavigateVard with StyleSheet styles

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,15 +1,42 @@
 import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import tw from "twrnc";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const NavigateCard = () => {
     return (
         <SafeAreaView style={tw`bg-white flex-1`}>
-            <Text>Dzien dobry Tomek</Text>
+            <Text style={tw`text-center py-5 text-xl`}>Dzień dobry, Tomek</Text>
+            <View style={tw`border-t border-gray-200 flex-shrink`}>
+                <View>
+                    <GooglePlacesAutocomplete
+                        placeholder='Where to?'
+                        styles={toInputBoxStyles}
+                        nearbyPlacesAPI='GooglePlacesSearch'
+                        debounce={400}
+                    />
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
 
 export default NavigateCard
 
-const styles = StyleSheet.create({})
+const toInputBoxStyles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        paddingTop: 20,
+        flex: 0,
+    },
+    textInput: {
+        backgroundColor: "#DDDDDF",
+        borderRadius: 0,
+        fontSize: 18,
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0,
+    }
+})
