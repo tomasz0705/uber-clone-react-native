f914a347e9e17b976c8ef26b68ffbd227c106561
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-01-17 15:10:53 +0100

    Added NavigateCard & RideOptionsCard to Stack.Screen at MapScreen.js

---
+++ components/NavigateCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import tw from "twrnc";
+
+const NavigateCard = () => {
+    return (
+        <SafeAreaView style={tw`bg-white flex-1`}>
+            <Text>Dzien dobry Tomek</Text>
+        </SafeAreaView>
+    )
+}
+
+export default NavigateCard
+
+const styles = StyleSheet.create({})
