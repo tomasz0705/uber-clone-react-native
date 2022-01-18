ffff7e64ac55a71148a190fd1aa2eb7020a56b71
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-01-18 17:40:38 +0100

    Added onPress with dispatching action payload, navigation, query attributes at NavigateCard.js

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -3,8 +3,14 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import tw from "twrnc";
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination } from '../slices/navSlice';
+import { useNavigation } from '@react-navigation/native';
 
 const NavigateCard = () => {
+    const dispatch = useDispatch();
+    const navigation = useNavigation();
+
     return (
         <SafeAreaView style={tw`bg-white flex-1`}>
             <Text style={tw`text-center py-5 text-xl`}>Dzień dobry, Tomek</Text>
@@ -13,6 +19,22 @@ const NavigateCard = () => {
                     <GooglePlacesAutocomplete
                         placeholder='Where to?'
                         styles={toInputBoxStyles}
+                        fetchDetails={true}
+                        returnKeyType={"search"}
+                        minLength={2}
+                        onPress={(data, details = null) => {
+                            dispatch(setDestination({
+                                location: details.geometry.location,
+                                description: data.description,
+                            }));
+
+                            navigation.navigate('RideOptionsCard');
+                        }}
+                        enablePoweredByContainer={false}
+                        query={{
+                            key: GOOGLE_MAPS_APIKEY,
+                            language: "pl",
+                          }}
                         nearbyPlacesAPI='GooglePlacesSearch'
                         debounce={400}
                     />
