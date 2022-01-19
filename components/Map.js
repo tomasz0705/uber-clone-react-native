61cbe84ab5eced77eb17d5101330b88345f8f330
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-01-19 20:39:24 +0100

    Added destination marker to MapView element at Map.js with re render component dependence on origin & destination

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { useSelector } from "react-redux";
@@ -10,9 +10,20 @@ import { GOOGLE_MAPS_APIKEY } from "@env";
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if(!origin || !destination) return;
+
+    // Zoom & fit to markers
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    });
+  }, [origin, destination])
 
   return (
     <MapView
+      ref={mapRef}
       style={tw`flex-1`}
       mapType="mutedStandard" //more subtle, makes markers/lines pop more (iOS 11.0+ only)
       initialRegion={{
@@ -31,6 +42,7 @@ const Map = () => {
           strokeColor="black"
         />
       )}
+
       {origin?.location && (
         <Marker
           coordinate={{
@@ -40,6 +52,18 @@ const Map = () => {
           title="Origin"
           description={origin.description}
           identifier="origin"
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title="Destination"
+          description={destination.description}
+          identifier="destination"
         />
       )}
     </MapView>
