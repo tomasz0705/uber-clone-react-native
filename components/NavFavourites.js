13470a69789f96c2a478f048d8d1a19defb4f2a9
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-01-20 18:05:25 +0100

    Added ItemSeparatorComponent (a thin line) combined tailwind with inline styles to FlatList

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Icon } from "react-native-elements";
 import tw from "twrnc";
 
@@ -8,20 +14,28 @@ const data = [
     id: "123",
     icon: "home",
     location: "Home",
-    destination: "Code Street, London, UK",
+    destination: "Warszawa, Polska",
   },
   {
     id: "456",
     icon: "briefcase",
     location: "Work",
-    destination: "London Eye, London, UK",
+    destination: "Stadion Narodowy, Aleja Księcia Józefa Poniatowskiego, Warszawa, Polska",
   },
 ];
 
 const NavFavourites = () => {
-  return <FlatList data={data} keyExtractor={(item) => item.id}
-  renderItem={({item: { location, destination, icon }}) => (
-      <TouchableOpacity>
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      ItemSeparatorComponent={() => (
+          <View
+            style={[tw`bg-gray-200`, { height: 0.5 }]}
+          />
+      )}
+      renderItem={({ item: { location, destination, icon } }) => (
+        <TouchableOpacity style={tw`flex-row items-center p-5`}>
           <Icon
             style={tw`mr-4 rounded-full bg-gray-300 p-3`}
             name={icon}
@@ -29,9 +43,14 @@ const NavFavourites = () => {
             color="white"
             size={18}
           />
-      </TouchableOpacity>
-  )}
-  />;
+          <View>
+            <Text style={tw`font-semibold text-lg`}>{location}</Text>
+            <Text style={tw`text-gray-500`}>{destination}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
 };
 
 export default NavFavourites;
