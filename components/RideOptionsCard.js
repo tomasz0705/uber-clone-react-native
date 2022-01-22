e2dd6a31bd881d5ceb8831cbaee53b58fa00f2c2
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-01-22 21:22:25 +0100

    Added back button using react-navigation, and tailwind styles to RideOptionsCard.js

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Icon } from "react-native-elements";
+import tw from "twrnc";
 
 const RideOptionsCard = () => {
-    return (
-        <View>
-            <Text>Pick a ride</Text>
-        </View>
-    )
-}
+  const navigation = useNavigation();
 
-export default RideOptionsCard
+  return (
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 p-3 rounded-full z-50`} //z-index is fixing the issue where back button is not clickable
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default RideOptionsCard;
+
+const styles = StyleSheet.create({});
