395bad1f3ded6ed4ed065f40c7ab6f4252f11d2f
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-01-23 22:26:31 +0100

    Added FlatList with data object of 3 kinds of uber rides to RideOptionsCard.js

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,6 +1,8 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import {
+  FlatList,
+  Image,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -9,6 +11,27 @@ import {
 } from "react-native";
 import { Icon } from "react-native-elements";
 import tw from "twrnc";
+
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-Van-456",
+    title: "Uber Van",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-Comfort-789",
+    title: "Uber Comfort",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
@@ -24,6 +47,30 @@ const RideOptionsCard = () => {
         </TouchableOpacity>
         <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
       </View>
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiplier, image}, item }) => (
+          <TouchableOpacity
+            style={tw`flex-row justify-between items-center px-10`}
+          >
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+              source={{ uri: image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>Travel time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>99zł</Text>
+          </TouchableOpacity>
+        )}
+      />
     </SafeAreaView>
   );
 };
